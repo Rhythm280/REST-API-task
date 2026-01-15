c843acbe4920b6b119b11cd79b52d69df9096e64
MDS <mds@MDSs-MacBook-Air.local>
2026-01-15 18:26:38 +0530

    minor bug fixing

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\CollectionController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AssetsController;
-use App\Http\Controllers\FilterAndSearchController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
